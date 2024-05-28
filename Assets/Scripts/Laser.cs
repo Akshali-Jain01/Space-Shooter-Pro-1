da080b6effb663510458f48ac5c5787307206bdb
Akshali Jain <akshali@harmonialaboriosa.com>
2024-05-28 11:42:08 +0530

    laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
